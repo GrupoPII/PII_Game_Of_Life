2d3f14863014b8e8dce55f2bdc8981f377f80f60
Valentina Hernández Rovere <valentinahernandezrovere@MacBook-Air-de-Valentina.local>
2026-08-26 09:49:59 -0300

    Importer pronto

--- src/Library/boardimporter.cs
+++ src/Library/boardimporter.cs
@@ -1,9 +1,37 @@
 using System;
+using System.ComponentModel.DataAnnotations;
 using System.IO;
 using System.Reflection;
 using System.Reflection.Metadata;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography.X509Certificates;
 
 namespace Ucu.Poo.GameOfLife
 {
-    string url = 
+    public static class BoardImporter
+    {
+        public static string Url { get; } = "src/Program/board.txt";
+
+        public static string Content { get; } = File.ReadAllText(Url);
+        public static string[] ContentLines { get; } = Content.Split('\n');
+
+        public static bool[,] Board { get; } = InitializeBoard();
+
+        private static bool[,] InitializeBoard()
+        {
+            bool[,] board = new bool[ContentLines.Length, ContentLines[0].Length];
+            for (int y = 0; y < ContentLines.Length; y++)
+            {
+                for (int x = 0; x < ContentLines[y].Length; x++)
+                {
+                    if (ContentLines[y][x] == '1')
+                    {
+                        board[x, y] = true;
+                    }
+                }
+            }
+            return board;
+        }
+
+    }
 }
